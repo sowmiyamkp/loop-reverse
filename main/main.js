8508c3e41db83477fb2178385cd52097b6c9aa68
sowmiyamkp <sowmiyamkp1305@gmail.com>
2023-03-25 21:54:13 -0700

    add loop-reverse

---
+++ main/main.js
@@ -0,0 +1,20 @@
+//reverse
+
+var ary=[1,2,3,4,5];
+for(i=ary.length-1;i>=0;i--)
+{
+	console.log(ary[i]);
+}
+
+
+//function reverse
+var array=[6,7,8,9,10];
+function reverseNum(array)
+{
+	for(i=array.length-1;i>=0;i--)
+	{
+		console.log(array[i]);
+		
+	}
+}
+reverseNum(array);
